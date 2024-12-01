8c940e37e0ccfbf92bbd68019ee68f3cd53bfc6d
Federico <federico.colantoni@euris.it>
2024-12-01 22:02:46 +0100

    feat: Day 1 completed

---
+++ Day1/Part1.cs
@@ -0,0 +1,34 @@
+﻿namespace Day1;
+
+public class Part1
+{
+    public int Solve()
+    {
+        var input = File.ReadAllLines("C:\\Projects\\AdventOfCodes\\AdventOfCode2024\\Day1\\input.txt");
+
+        var listA = new List<int>();
+        var listB = new List<int>();
+
+        var distance = 0;
+
+        foreach (var item in input)
+        {
+            var split = item.Split("   ");
+            listA.Add(int.Parse(split[0]));
+            listB.Add(int.Parse(split[1]));
+        }
+
+        while (listA.Any() && listB.Any())
+        {
+            var itemA = listA.Min();
+            var itemB = listB.Min();
+            
+            distance += Math.Abs(itemA - itemB);
+            
+            listA.Remove(itemA);
+            listB.Remove(itemB);
+        }
+
+        return distance;
+    }
+}
---
+++ Day1/Part2.cs
@@ -0,0 +1,28 @@
+﻿namespace Day1;
+
+public class Part2
+{
+    public int Solve()
+    {
+        var input = File.ReadAllLines("C:\\Projects\\AdventOfCodes\\AdventOfCode2024\\Day1\\input.txt");
+
+        var listA = new List<int>();
+        var listB = new List<int>();
+
+        var distance = 0;
+
+        foreach (var item in input)
+        {
+            var split = item.Split("   ");
+            listA.Add(int.Parse(split[0]));
+            listB.Add(int.Parse(split[1]));
+        }
+
+        foreach (var item in listA)
+        {
+            distance += listB.FindAll(x => x == item).Count * item;
+        }
+
+        return distance;
+    }
+}
