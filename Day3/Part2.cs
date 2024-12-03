81c47ed46d32ec85f8cc6ceafa0d178987479cfe
Federico <federico.colantoni@euris.it>
2024-12-03 22:56:44 +0100

    feat: Day 3 implementation
    Part 2 completed.

---
+++ Day3/Part2.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public class Part2
+{
+    public int Solve()
+    {
+        var result = 0;
+        var input = File.ReadAllLines("C:\\Projects\\AdventOfCodes\\AdventOfCode2024\\Day3\\input.txt");
+
+        var multiplyRegexPattern = @"(do\(\)|don't\(\))|(mul\([0-9]{1,3},[0-9]{1,3}\))";
+        var extractNumbersRegexPattern = @"\b[0-9]{1,3},[0-9]{1,3}\b";
+        var doString = @"do()";
+        var dontString = @"don't()";
+
+        var enableMultiply = true;
+
+        foreach (var item in input)
+        {
+            var validMultiply = Regex.Matches(item, multiplyRegexPattern).ToList();
+            
+            while (validMultiply.Count > 0)
+            {
+                var match = validMultiply[0];
+
+                if (match.Value == dontString)
+                {
+                    enableMultiply = false;
+                    validMultiply.RemoveAt(0);
+                    continue;
+                }
+                if(match.Value == doString)
+                {
+                    enableMultiply = true;
+                    validMultiply.RemoveAt(0);
+                    continue;
+                }
+
+                if (enableMultiply)
+                {
+                    if (Regex.Match(match.Value, extractNumbersRegexPattern).Success)
+                    {
+                        var value = Regex.Match(match.Value, extractNumbersRegexPattern).Value.Split(",");
+
+                        result += int.Parse(value[0]) * int.Parse(value[1]);
+                    }
+                    
+                    validMultiply.RemoveAt(0);
+                }
+                else
+                {
+                    validMultiply.RemoveAt(0);
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- Day3/Program.cs
+++ Day3/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Part 1 = " + new Part1().Solve());
+        Console.WriteLine("Part 2 = " + new Part2().Solve());
     }
 }
